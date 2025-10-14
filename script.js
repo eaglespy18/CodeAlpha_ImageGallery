7baa5d8593ef0d2b9c2844cd69c76a399e9cbd83
mo <alhassankhidir@gmail.com>
2025-10-14 07:04:46 -0700

    Initial commit - CodeAlpha Image Gallery

---
+++ script.js
@@ -0,0 +1,34 @@
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+const images = document.querySelectorAll(".gallery-item img");
+
+let currentIndex = 0;
+
+images.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+    currentIndex = index;
+  });
+});
+
+closeBtn.addEventListener("click", () => (lightbox.style.display = "none"));
+
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  lightboxImg.src = images[currentIndex].src;
+});
+
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  lightboxImg.src = images[currentIndex].src;
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
